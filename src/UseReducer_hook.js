f9cffea612723c574cd155ad0396da3e52ba8f39
unknown <r.bobrowicz@live.com>
2022-11-17 19:56:57 +0100

    useReducer

--- src/UseReducer_hook.js
+++ src/UseReducer_hook.js
@@ -3,42 +3,81 @@ import { useReducer } from 'react';
 
 export const UseReducerHook = () => {
 
-    const [state, dispatch] = useReducer((state, action) => {
-        switch (action.type) {
-            case 'SET_NAME':
-                return {
-                    ...state,
-                    name: action.payload
-                };
-            case 'ADD_NAME':
-                return {
-                    ...state,
-                    names: [...state.names, action.payload],
-                    name: ''
-                };
-            default: return { ...state };
-        }
-    }, {
-        names: [],
-        name: ''
-    })
+    function UserForm() {
+        const [state, dispatch] = useReducer((state, action) => {
+            return {
+                ...state,
+                ...action
+            }
+        }, {
+            firstName: '',
+            lastName: ''
+        });
+
+        return (
+            <div>
+                <input
+                    type="text"
+                    value={state.firstName}
+                    onChange={e => dispatch({ firstName: e.target.value })}
+                    placeholder="Enter first name"
+                />
+                <input
+                    type="text"
+                    value={state.lasttName}
+                    onChange={e => dispatch({ lastName: e.target.value })}
+                    placeholder="Enter last name"
+                />
+                <div>Your name: {state.firstName} {state.lastName}</div>
+            </div>
+        )
+    };
+
+    function UserList() {
+        const [state, dispatch] = useReducer((state, action) => {
+            switch (action.type) {
+                case 'SET_NAME':
+                    return {
+                        ...state,
+                        name: action.payload
+                    };
+                case 'ADD_NAME':
+                    return {
+                        ...state,
+                        names: [...state.names, action.payload],
+                        name: ''
+                    };
+                default: return { ...state };
+            }
+        }, {
+            names: [],
+            name: ''
+        })
+
+        return (
+            <div>
+                <h2>useReducer</h2>
+                {state.names.map((el, indx) => (
+                    <div key={indx} >
+                        {el}
+                    </div>
+                ))}
+                <input
+                    type="text"
+                    value={state.name}
+                    onChange={e => dispatch({ type: 'SET_NAME', payload: e.target.value })}
+                    placeholder="Enter name"
+                />
+                <div>You just typed: {state.name}</div>
+                <button onClick={() => dispatch({ type: 'ADD_NAME', payload: state.name })}>Add name</button>
+            </div>
+        )
+    };
 
     return (
         <div>
-            <h2>useReducer</h2>
-            {state.names.map((el, indx) => (
-                <div key={indx} >
-                    {el}
-                </div>
-            ))}
-            <input
-                type="text"
-                value={state.name}
-                onChange={e => dispatch({ type: 'SET_NAME', payload: e.target.value })}
-                placeholder="Enter name"
-            />
-            <div>You just typed: {state.name}</div>
-            <button onClick={() => dispatch({ type: 'ADD_NAME', payload: state.name })}>Add name</button>
+            <UserList />
+            <UserForm />
         </div>
     )
 }
