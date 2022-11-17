c1bd932e68b61c09d4fee12d127d74eb7eb33785
unknown <r.bobrowicz@live.com>
2022-11-17 20:36:00 +0100

    useReducer

--- src/UseMemo_hook.js
+++ src/UseMemo_hook.js
@@ -1,7 +1,17 @@
+import { useState, useMemo } from 'react';
+
 function UseMemo() {
+    const [numbers] = useState([2, 3, 5]);
+    const total = useMemo(
+        () => numbers.reduce((acc, number) => acc + number, 0),
+        [numbers]
+    );
+
     return (
         <div>
             <h2>useMemo hook</h2>
+            <div>Array of numbers: {numbers.join(", ")}</div>
+            <div>Total: {total}</div>
         </div>
     )
 }
