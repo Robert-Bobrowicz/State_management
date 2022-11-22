6dd4a363bd93304b9c089da44108880a6f3ee931
unknown <r.bobrowicz@live.com>
2022-11-22 15:02:40 +0100

    useCallback

--- src/UseCallback_hook.js
+++ src/UseCallback_hook.js
@@ -1,8 +1,27 @@
+import { useState, useMemo, useCallback } from "react";
+
 function UseCallback() {
+    const [plusOne, setPlusOne] = useState(0);
+    const [minusOne, setMinusOne] = useState(0);
+
+    const [names] = useState(["Kasia", "Ania", "Ola", "Nina", "Sandra", "Julia", "Marek", "Janek", "Pjoter", "Paweł", "King", "Rysio", "Magda", "Anastazja", "Kala", "Olek", "Adam", "Sasza"]);
+    // const sortedNames = useMemo(
+    //     () => [...names].sort(sortFunc), [names]
+    // );
+
+    const sortFunc = (a, b) => a.localeCompare(b) * -1; //sotowanie odwrotne
+
+    function SortedNames({ list }) {
+        const sortedNames = useMemo(() => [...list].sort(sortFunc), [list]);
+        return <div>Sorted names in reverse order: [ {sortedNames.join(", ")} ]</div>;
+    };
 
     return (
         <div>
             <h2>useCallback hook</h2>
+            <SortedNames list={names} />
+            <button onClick={() => setPlusOne(plusOne + 1)}> +1: {plusOne}</button>
+            <button onClick={() => setMinusOne(minusOne - 1)}> -1: {minusOne}</button>
         </div>
     )
 }
